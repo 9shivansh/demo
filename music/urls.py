3a46b68811e752f1aadcf641af7c16bb039d85d8
Shivansh <9shivanshsharma@gmail.com>
2020-09-16 16:54:38 +0530

    Created the functionality for uploading files
    As knowledge about forms is not sufficient, we tried to upload the files int media using FileSystemStorage

--- music/urls.py
+++ music/urls.py
@@ -5,4 +5,5 @@ from music import views
 
 urlpatterns = [
     path('', views.music_home, name = 'music_home'),
+    path('upload/', views.upload, name = 'upload'),
 ]
--- music/views.py
+++ music/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
+from django.core.files.storage import FileSystemStorage
+
+
+#from .forms import UploadFileForm
+
+# Imaginary function to handle an uploaded file.
+#from somewhere import handle_uploaded_file
+
+def upload(request):
+    if request.method == 'POST':
+        uploaded_file = request.FILES['file']
+        fs = FileSystemStorage()
+        fs.save(uploaded_file.name, uploaded_file)
+    return render(request, 'upload.html')
 
 # Create your views here.
 def music_home(request):
 
     context = {
-        
+
     }
 
     return render(request, 'music_home.html', context)
