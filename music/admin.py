e4f6a722d4abd03457c5a985ec5799e23013284c
Shivansh <9shivanshsharma@gmail.com>
2020-09-16 14:10:58 +0530

    Created the song model
    Created the song model along with migrating it for the database and also made it accessible from the admin page

--- music/admin.py
+++ music/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from music import models
 
 # Register your models here.
+admin.site.register(models.song)
--- music/models.py
+++ music/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class song(models.Model):
+
+    song_title = models.TextField(max_length = '250')
+    song_artist = models.TextField(max_length = '100')
+    song_album = models.TextField(max_length = '100')
+    song_url = models.TextField(max_length = '500')
+
+    def __str__(self):
+
+        return self.song_title + ' by ' + self.song_artist + ' from the album - ' + self.song_album
