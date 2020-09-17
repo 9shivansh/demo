c2a16bc7cbc9639772c63955d6d3a304411578c3
Shivansh <9shivanshsharma@gmail.com>
2020-09-17 10:08:19 +0530

    Deleting the files from Media Root
    Previous problem was that even after deleting the file, it was stil present there in the media root, fixed that

--- music/models.py
+++ music/models.py
@@ -13,3 +13,9 @@ class song(models.Model):
     def __str__(self):
 
         return self.song_title + ' by ' + self.song_artist + ' from the album - ' + self.song_album
+
+
+    def delete(self, *args, **kwargs):
+
+        self.song_file.delete()
+        super().delete(*args, **kwargs)
